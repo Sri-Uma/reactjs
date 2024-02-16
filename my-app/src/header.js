6b14b49c63ef3a74a95b5d88a7dfed84c3c224fb
Sri Uma <vundavallisriuma666@gamail.com>
2024-02-16 14:55:40 +0530

    super over league

---
+++ my-app/src/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => (
+<div className="bg-container">
+    <h1 className="heading">Super Over League</h1>
+    <div className="images">
+    <img src = "https://assets.ccbp.in/frontend/react-js/rcb-img.png" className="img-1" alt="img-1"/>
+    <img src = "https://assets.ccbp.in/frontend/react-js/csk-img.png" className="img-2" alt="img-2"/>
+
+    </div>
+</div>
+);
+
+export default Header;
